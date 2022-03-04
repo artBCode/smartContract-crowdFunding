ea9fd45fa40e2bf9b03d505b0e6f9ab98656a28c
Daniel <danielg@Daniels-MacBook-Pro.local>
2022-03-04 07:26:57 -0600

    added links in the header

--- components/header.js
+++ components/header.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+import { Link } from '../routes'
 
 export default () => {
     return (
-       <Menu style={{ marginTop: '10px'}}>
-           <Menu.Item>
-                CrowdFunding
-           </Menu.Item>
+        <Menu style={{ marginTop: '10px' }}>
+            <Link route='/'>
+                <a className='item'>CrowdFunding</a>
+            </Link>
 
-           <Menu.Menu position='right' >
-               <Menu.Item>
-                   Campaigns
-               </Menu.Item>
-               <Menu.Item>
-                   <b>+</b>
-               </Menu.Item>
-           </Menu.Menu>
-       </Menu> 
+            <Menu.Menu position='right' >
+                <Link route='/'>
+                    <a className='item'>Campaigns</a>
+                </Link>
+
+                <Link route='/campaigns/new'>
+                    <a className='item'>+</a>
+                </Link>
+
+            </Menu.Menu>
+        </Menu>
     );
 }
